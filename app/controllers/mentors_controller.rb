3248d47c02bbc1bf01a2bc0a924fb15ca55d05e3
Marcela Andrade <marcelasrandrade@gmail.com>
2024-11-09 20:47:57 +0000

    criar a  mentora

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -1,9 +1,28 @@
 class MentorsController < ApplicationController
   def index
-    @mentors = Mentor.all #exibindo uma lista de mentoras
+    @mentors = Mentor.all # exibindo uma lista de mentoras
   end
 
   def show
-    @mentor = Mentor.find(params[:id]) #exibindo uma mentora com id do paramentro
+    @mentor = Mentor.find(params[:id]) # exibindo uma mentora com id do paramentro
+  end
+
+  def new
+    @mentor = Mentor.new
+  end
+
+  def create
+    @mentor = Mentor.new(mentor_params)
+
+    if @mentor.save
+      redirect_to @mentor
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def mentor_params
+    params.expect(mentor: [ :name, :email, :skills, :age, :phone ])
   end
 end
