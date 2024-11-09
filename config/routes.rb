adfa87d39d2d503a1f5d08b5106c832d807574fc
Ggabrielasantos <119715154+Ggabrielasantos@users.noreply.github.com>
2024-11-09 19:15:27 +0000

    criação do modelo do banco de dados

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
+  root "mentors#index"
   get "/mentors", to: "mentors#index"
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
