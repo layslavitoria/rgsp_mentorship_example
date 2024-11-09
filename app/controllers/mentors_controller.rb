c72452c8cd10943a46baa762e2265caae8b74193
Marcela Andrade <marcelasrandrade@gmail.com>
2024-11-09 20:22:07 +0000

    criar rota para exibir uma mentora

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -1,5 +1,9 @@
 class MentorsController < ApplicationController
   def index
-    @mentors = Mentor.all
+    @mentors = Mentor.all #exibindo uma lista de mentoras
+  end
+
+  def show
+    @mentor = Mentor.find(params[:id]) #exibindo uma mentora com id do paramentro
   end
 end
